41129bc58d350bb228d0fa2bd19b70c6694c8962
Mark Wales <mark@smallhadroncollider.com>
2019-01-10 10:59:42 +0000

    fix (Brok): link parsing with brackets

--- src/Brok.hs
+++ src/Brok.hs
@@ -6,26 +6,39 @@ module Brok
     , links
     ) where
 
-import ClassyPrelude hiding ((<|>))
+import ClassyPrelude
 
 import Data.Either (fromRight)
 
-import Text.Parsec
+import Text.Parsec      (alphaNum, anyToken, char, many1, oneOf, optionMaybe, parse, string)
 import Text.Parsec.Text (Parser)
 
 type Link = Text
 
 type Token = Maybe Link
 
-urlChars :: Parser Char
-urlChars = alphaNum <|> oneOf "-._~:/?#%[]@!$&'()*+,;="
+surround :: Char -> Char -> Parser Text -> Parser Text
+surround open close parser = do
+    o <- singleton <$> char open
+    fragment <- parser
+    c <- singleton <$> char close
+    return $ o ++ fragment ++ c
+
+parens :: Parser Text -> Parser Text
+parens parser = surround '(' ')' parser <|> surround '[' ']' parser
+
+urlChar :: Parser Char
+urlChar = alphaNum <|> oneOf "-._~:/?#%@!$&*+,;="
+
+urlChars :: Parser Text
+urlChars = concat <$> many1 (parens urlChars <|> (pack <$> many1 urlChar))
 
 url :: Parser Token
 url = do
     protocol <- pack <$> string "http"
     secure <- maybe "" singleton <$> optionMaybe (char 's')
     slashes <- pack <$> string "://"
-    address <- pack <$> many1 urlChars
+    address <- urlChars
     return . Just $ concat [protocol, secure, slashes, address]
 
 noise :: Parser Token
@@ -34,8 +47,8 @@ noise = anyToken >> return Nothing
 urls :: Parser [Link]
 urls = catMaybes <$> many1 (url <|> noise)
 
-links :: Text -> [Link]
-links txt = fromRight [] $ parse urls "(unknown)" txt
+links :: FilePath -> Text -> [Link]
+links path txt = fromRight [] $ parse urls path txt
 
 brok :: IO ()
 brok = putStrLn "brok"
--- test/Spec.hs
+++ test/Spec.hs
@@ -23,33 +23,67 @@ tests =
                     (assertEqual
                          "Gives back google.com"
                          ["http://google.com"]
-                         (links "http://google.com"))
+                         (links "(test: Spec.hs)" "http://google.com"))
               , testCase
                     "just https link"
                     (assertEqual
                          "Gives back google.com"
                          ["https://google.com"]
-                         (links "https://google.com"))
+                         (links "(test: Spec.hs)" "https://google.com"))
               , testCase
                     "http link with surrounding text"
                     (assertEqual
                          "Gives back google.com"
                          ["http://google.com"]
-                         (links "A link to Google http://google.com - doesn't it look nice"))
+                         (links
+                              "(test: Spec.hs)"
+                              "A link to Google http://google.com - doesn't it look nice"))
               , testCase
                     "https link with surrounding text"
                     (assertEqual
                          "Gives back google.com"
                          ["https://google.com"]
-                         (links "A link to Google https://google.com - doesn't it look nice"))
-              , expectFail $
-              -- will fail as URLs can contain brackets
-                testCase
+                         (links
+                              "(test: Spec.hs)"
+                              "A link to Google https://google.com - doesn't it look nice"))
+              , testCase
                     "markdown"
                     (assertEqual
                          "Gives back google.com"
                          ["http://google.com"]
-                         (links "[A link](http://google.com)"))
+                         (links "(test: Spec.hs)" "[A link](http://google.com)"))
+              , testCase
+                    "link with brackets"
+                    (assertEqual
+                         "Gives back google.com"
+                         ["http://google.com?q=(fish)"]
+                         (links "(test: Spec.hs)" "[A link](http://google.com?q=(fish))"))
+              , testCase
+                    "link with square brackets surrounding"
+                    (assertEqual
+                         "Gives back google.com"
+                         ["http://google.com?q=(fish)"]
+                         (links "(test: Spec.hs)" "[http://google.com?q=(fish)]"))
+              , testCase
+                    "link with square brackets"
+                    (assertEqual
+                         "Gives back google.com"
+                         ["http://google.com?q=fish"]
+                         (links "(test: Spec.hs)" "[http://google.com?q=fish]"))
+              , expectFail $
+                -- using surround with the same character on each side causing issues? 
+                testCase
+                    "link with single quotes"
+                    (assertEqual
+                         "Gives back google.com"
+                         ["http://google.com?q='fish'"]
+                         (links "(test: Spec.hs)" "http://google.com?q='fish'"))
+              , testCase
+                    "link with single quotes surrounding"
+                    (assertEqual
+                         "Gives back google.com"
+                         ["http://google.com?q=fish"]
+                         (links "(test: Spec.hs)" "'http://google.com?q=fish'"))
               ]
         , testGroup
               "multiple links"
@@ -59,6 +93,7 @@ tests =
                          "Gives back google.com"
                          ["https://google.com", "http://spoons.com"]
                          (links
+                              "(test: Spec.hs)"
                               "A link to Google https://google.com - doesn't it look like http://spoons.com"))
               ]
         ]
